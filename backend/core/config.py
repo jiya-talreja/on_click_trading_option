0594d5390d10311971688623145b6fd92cc2a174
jiya-talreja <talrejajiya115@gmail.com>
2026-07-12 18:38:34 +0530

    Refactor config.py to include Redis settings
    Added redis configuration parameters and improved formatting.

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,11 +1,16 @@
 from pydantic import ValidationError
 from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
-    client_key:str
-    token_access:str
+    client_key: str
+    token_access: str
+    redis_host:str
+    redis_port:int
+    redis_db:int
+    redis_timeout:int
+    redis_max_connection:int
     class Config:
-        env_file=".env"
+        env_file = ".env"
 try:
-    settings=Settings()
+    settings = Settings()
 except ValidationError as e:
     raise RuntimeError(f"Configuration error:\n{e}")
