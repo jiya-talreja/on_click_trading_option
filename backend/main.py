7fcbdf1834db369eea44f31b9a3defd6b3f2a2fe
jiya-talreja <talrejajiya115@gmail.com>
2026-07-15 23:36:42 +0530

    Refactor trading logic and add WebSocket support
    Refactored trading logic to utilize services for account, quantity, stop loss, validation, and order management. Added WebSocket endpoint for trade streaming.

--- backend/main.py
+++ backend/main.py
@@ -2,17 +2,17 @@ from fastapi import FastAPI,HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pandas as pd
-import threading#for running multiple things at once
-from dhanhq import marketfeed#module from the function that has the websockter
-from dhanhq import DhanContext,MarketFeed
+from services.account_info import AccountService
 import requests
-print(requests.get("https://api.dhan.co").status_code)
-client_key=""
-token_access=""
-dhancontext=DhanContext(
-    client_key,token_access
-)
+from fastapi import WebSocket, WebSocketDisconnect
 
+from services.quantity_info import QuantityInfo
+from services.stoploss import StopLossDync
+from services.validation import ValidationService
+from services.order_service import OrderService
+from services.position_service import PositionService
+from storage.redis_storage import save_position,get_position
+CAPITAL_PERCENTAGE = 90
 app=FastAPI(title="Tradingview-backend")
 app.add_middleware(
     CORSMiddleware,
@@ -25,82 +25,74 @@ df=pd.read_csv("api-scrip-master.csv")
 token_lookup=dict(
     zip(df['SEM_TRADING_SYMBOL'].str.upper(),df['SEM_SMST_SECURITY_ID'])
     )#hashmap or dictionary from two list club together by zip
-
-#on connection successful,dhan atomatically gives a call so async func for that
-def on_connect_established(instance):
-    print("connection successful,pipeline connected")
-    print(instance)
-
-#thhis has the dictionary which has LTP continuous answer
-def on_price_update_received(instance, message):
-     print(instance)
-     print(message)
-     if message.get('type') == "Ticker Data":
-        
-        # 2. Extract the current market price using the 'LTP' key
-        current_market_price = message.get('LTP')
-        
-        print(f"Current Market Price: {current_market_price}")
-
-def on_error(instance, error):
-    print("=" * 80)
-    print("ERROR CALLBACK")
-    print(type(error))
-    print(error)
-    import traceback
-    traceback.print_exception(type(error), error, error.__traceback__)
-    print("=" * 80)
-
-#actual call to websocket
-def start_websocket(secur_id : int):
-    try:
-        print(f"RUNNING THREAD {secur_id}")
-        print(secur_id)
-        print(type(secur_id))
-        target_instruments=[(MarketFeed.NSE,str(secur_id),MarketFeed.Ticker),]#NSC EQUITY,ID OF SHARE
-        print(target_instruments)
-        subscription_mode=2#INSTEAD OF EVEryTHING GIVE ME SPECIFIC THINGS ONLY
-        feed = marketfeed.MarketFeed(
-            dhan_context=dhancontext,
-            instruments=target_instruments,
-            version="v2",
-            on_connect=on_connect_established,
-            on_message=on_price_update_received
-        )
-        feed.run_forever()
-    except Exception as e:
-        print("Websocket Error:", e)
-        import traceback
-        traceback.print_exc()
-
 class Tradeinput(BaseModel):
     stock:str
     action:str
+    price:str
 @app.post("/trade")
 async def inputs(payloads : Tradeinput):
     try:
         stock_name=payloads.stock.upper()
         action_side=payloads.action
-        print(stock_name,action_side)
+        price_current=float(payloads.price)
+
         secur_id=token_lookup.get(stock_name)
         if not secur_id:
             print("COULDNT GET SECUR_ID")
-        print("SECUR ID : ",secur_id,"stock_name",stock_name)
 
-        print("THREAD STARTING")
-        threading.Thread(
-            target=start_websocket, 
-            args=(secur_id,), 
-            daemon=True
-        ).start()
-
-        print("THREAD DONE")
-        
-        return {
-            "secur_id":secur_id,
+        trade_context={
             "stock":stock_name,
             "action":action_side,
-            "message":"PIPELINE CONNECTED"
+            "cp":price_current,
+            "stock_id":secur_id
+        }
+
+        account_service = AccountService()    
+        funds=account_service.get_funds() 
+        avail_amount=funds["data"]["availabelBalance"]  
+        trade_context["balance"]=avail_amount
+        print("FUNDS : ",funds)
+
+        quantity_service=QuantityInfo()
+        quantity=quantity_service.calculate_quantity(avail_amount,price_current,CAPITAL_PERCENTAGE)
+        print(quantity)
+        trade_context["quantity"]=quantity
+
+        stoploss_service=StopLossDync()
+        print(stoploss_service.stoploss(action_side,price_current,1))
+        trade_context["stoploss"]=stoploss_service.stoploss(action_side,price_current,1)
+        trade_context["tsl"]=stoploss_service.initialtsl(action_side,price_current,1)
+
+        validate_service=ValidationService()
+        if_true=validate_service.validate(trade_context)
+        print(if_true)
+        if(if_true):
+            order_service=OrderService()
+            trade_context=order_service.mock_order(trade_context)
+            print("update trade_context")
+            print(trade_context)
+        if(trade_context["order_status"]=="FILLED"):
+            print("a")
+            position_service=PositionService()
+            position=position_service.create_position(trade_context)
+            print("psotion created : ",position)
+            save_position(position)
+            saved_position = get_position(position["position_id"])
+            print(saved_position)
+            trade_context["position_id"] = position["position_id"]
+        return {
+            "trade":trade_context
         }
     except Exception as e:
         raise HTTPException(status_code=500,detail=str(e))
+
+@app.websocket("/trade-stream")
+async def trade_stream(websocket: WebSocket):
+    await websocket.accept()
+    print("Extension Connected")
+    try:
+        while True:
+            data = await websocket.receive_json()
+            print("PRICE UPDATE :", data)
+    except WebSocketDisconnect:
+        print("Extension Disconnected")
