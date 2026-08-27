793e65293943c5950ea5da86c54d480e90dd1b7a
jiya-talreja <talrejajiya115@gmail.com>
2026-08-28 00:21:03 +0530

    Add manual exit and LTP stream routers

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,11 @@ import asyncio
 from background.market_closed import time_market_closed
 from routers.trade import router as trade_router
 from routers.stream import router as stream_router
+from routers.manual_exit import router as manual_router
+from routers.ltp_stream import stop_websocket
+from routers.ltp_stream import start_websocket
+import asyncio
+event_loop=None
 app=FastAPI(title="Tradingview-backend")
 app.add_middleware(
     CORSMiddleware,
@@ -14,6 +19,15 @@ app.add_middleware(
 )
 app.include_router(trade_router)
 app.include_router(stream_router)
+app.include_router(manual_router)
+
 @app.on_event("startup")
-async def start_market_checker():
+async def startup():
+    
+    start_websocket()
+    print("websocket")
     asyncio.create_task(time_market_closed())
+@app.on_event("shutdown")
+async def shutdown():
+
+    stop_websocket()
