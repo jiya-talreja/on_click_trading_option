e0a4b064210fa860bfc6002c8e77d842d1178463
jiya-talreja <talrejajiya115@gmail.com>
2026-08-28 00:26:06 +0530

    Refactor stoploss methods to use ATR component

--- backend/services/stoploss.py
+++ backend/services/stoploss.py
@@ -1,27 +1,29 @@
+import pandas as pd
 class StopLossDync:
     def stoploss(
         self,
         action: str,
         price: float,
-        stop_percent: float
+        atr_component: float
     ):
-        inter = price * (stop_percent / 100)
+        multiplier=1
+        atr_buffer = atr_component*multiplier
         if action == "BUY":
-            return round(price - inter, 2)
+            return round(price - atr_buffer,2)
         elif action == "SELL":
-            return round(price + inter, 2)
+            return round(price + atr_buffer,2)
         else:
             raise ValueError("Action must be BUY or SELL")
     def initialtsl(
         self,
         action: str,
         price: float,
-        stop_percent: float
+        atr_component: float
     ):
         return self.stoploss(
             action,
             price,
-            stop_percent
+            atr_component
         )
     def update_trailing_stop(
         self,
@@ -29,21 +31,24 @@ class StopLossDync:
         current_price: float,
         highest_price: float,
         lowest_price: float,
-        trailing_percent: float
+        atr_component: float
     ):
+        action = action.upper()
+        multiplier=1
+        atr_buffer = atr_component*multiplier
         if action == "BUY":
             if current_price > highest_price:
                 highest_price = current_price
-            tsl = highest_price * (1 - trailing_percent / 100)
+            tsl = highest_price - atr_buffer
             return round(tsl, 2), highest_price
         elif action == "SELL":
             if current_price < lowest_price:
                 lowest_price = current_price
-            tsl = lowest_price * (1 + trailing_percent / 100)
+            tsl = lowest_price + atr_buffer
             return round(tsl, 2), lowest_price
         else:
             raise ValueError("Action must be BUY or SELL")
-    def exit_check(
+    def exit_check_tsl(
         self,
         action: str,
         current_price: float,
@@ -51,8 +56,37 @@ class StopLossDync:
     ):
         action = action.upper()
         if action == "BUY":
-            return current_price <= trailing_stop
+            return {
+            "exit": current_price <= trailing_stop,
+            "reason": "TRAILING_STOP"
+            }
         elif action == "SELL":
-            return current_price >= trailing_stop
+            return {
+            "exit": current_price >= trailing_stop,
+            "reason": "TRAILING_STOP"
+            }
         else:
             raise ValueError("Action must be BUY or SELL")
+    def calculate_atr_from_candles(self,raw_candles,mean_period=14):
+        print("inside function")
+        if not raw_candles or len(raw_candles)<=mean_period:
+            print("candle return issue")
+            return None
+        print(raw_candles)
+        df=pd.DataFrame(raw_candles,columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'oi'])
+        #reveersing from past to present 
+        df=df.iloc[::-1].reset_index(drop=True)
+        print("reverse")
+        df['high']=df['high'].astype(float)
+        df['low']=df['low'].astype(float)
+        df['close']=df['close'].astype(float)
+
+        df['h_m_l']=df['high']-df['low']
+        df['h_m_c']=(df['high']-df['close'].shift(1)).abs()#look at one above for close(previous close)
+        df['l_m_c']=(df['low']-df['close'].shift(1)).abs()
+
+        df['tr']=df[['h_m_l','h_m_c','l_m_c']].max(axis=1)#select 3 cols do row wise max
+        df['atr']=df['tr'].ewm(alpha=1 / mean_period, adjust=False).mean()
+        latest_atr=round(df['atr'].iloc[-1],2)
+        print(latest_atr)
+        return latest_atr
