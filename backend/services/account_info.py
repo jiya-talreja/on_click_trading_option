94117ed40b7681d1d7f313e0d2f7e690ebdd118a
jiya-talreja <talrejajiya115@gmail.com>
2026-08-28 00:23:12 +0530

    Fix access token assignment in AccountService

--- backend/services/account_info.py
+++ backend/services/account_info.py
@@ -5,18 +5,20 @@ from dhanhq import DhanContext
 class AccountService:
     def __init__(self):
         self.client_id = settings.client_key
-        self.access_token = settings.token_access
+        self.access_token = settings.access_token
         self.headers = {
             "client-id": self.client_id,
             "access-token": self.access_token
         }
-        context = DhanContext(settings.client_key,settings.token_access)
+        context = DhanContext(settings.client_key,self.access_token)
         self.dhan = dhanhq(context)
+        print("DHAN : ",self.dhan)
         print("Account Service Initialized")
     def get_funds(self):
         print("Calling Dhan Funds API")
         response=self.dhan.get_fund_limits()
         print(response)
         return response
+        
 
         
